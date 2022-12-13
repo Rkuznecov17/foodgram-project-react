e46de05909350ceec09f1abcf31a0fed01f6cc3d
Роман Федченко <kuznecovromanv@yandex.ru>
2022-12-13 14:43:35 +0300

    12.12.22

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
     'djoser',
     'recipes.apps.RecipesConfig',
     'colorfield',
-    
 ]
 
 MIDDLEWARE = [
@@ -105,7 +104,6 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
