abc96095b5ac5799786c85bdda0c5c8b839cc1c6
Роман Федченко <kuznecovromanv@yandex.ru>
2022-12-13 14:33:18 +0300

    13.12.22

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -1,13 +1,21 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = '2!4itka3+ub%--aq+w$25)9z9yzaksp@($c5s&l_iha_m^5z&f'
+SECRET_KEY = os.getenv('SECRET_KEY', default='2!4itka3+ub%--aq+w$25)9z9yzaksp@($c5s&l_iha_m^5z&f')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    '158.160.5.159',
+    'localhost',
+    'web',
+]
 
 AUTH_USER_MODEL = 'users.User'
 
@@ -62,8 +70,12 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.getenv('DB_HOST', default='db'),
+        'PORT': os.getenv('DB_PORT', default='5432')
     }
 }
 
@@ -93,7 +105,8 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
