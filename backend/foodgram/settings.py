6602258894c3f83d1eea46a66f908f2fb728d159
Роман Федченко <kuznecovromanv@yandex.ru>
2022-12-16 12:41:12 +0300

    final_36

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='2!4itka3+ub%--aq+w$25)9z9yzaksp@($c5s&l_iha_m^5z&f')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
